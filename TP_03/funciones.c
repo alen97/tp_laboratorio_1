d363ab838f6ce5f29bf64d5efe82a4395060114b
Gabriel <gaby.sur@hotmail.com>
2016-05-27 15:02:50 -0300

    Arreglos TP3

--- TP_03/funciones.c
+++ TP_03/funciones.c
@@ -234,10 +234,12 @@ void borrarPelicula(EMovie* movie)
 
 void generarPagina(EMovie* movie, char* nombre)
 {
-     FILE *pArch, *pFijoUno, *pFijoDos;
-     char fijoUno[512], fijoDos[512], dinamico[512], dinamicAux[512], textoFinal[512];
+     FILE *pArch;
+     char fijoUno[] = "<!DOCTYPE html><!-- Template by Quackit.com --><html lang='en'><head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1'><!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags --><title>Lista peliculas</title><!-- Bootstrap Core CSS --><link href='css/bootstrap.min.css' rel='stylesheet'><!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles --><link href='css/custom.css' rel='stylesheet'><!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --><!-- WARNING: Respond.js doesn't work if you view the page via file:// --><!--[if lt IE 9]><script src='https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js'></script><script src='https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'></script><![endif]--></head><body><div class='container'><div class='row'>";
+     char fijoDos[] = "</div><!-- /.row --></div><!-- /.container --><!-- jQuery --><script src='js/jquery-1.11.3.min.js'></script><!-- Bootstrap Core JavaScript --><script src='js/bootstrap.min.js'></script><!-- IE10 viewport bug workaround --><script src='js/ie10-viewport-bug-workaround.js'></script><!-- Placeholder Images --><script src='js/holder.min.js'></script></body></html>";
+     char dinamico[512], dinamicAux[512];
      int i;
-     short int hayDatos=0;
+     short int hayDatos = 0;
 
      pArch = fopen(nombre, "w");
 
@@ -251,7 +253,7 @@ void generarPagina(EMovie* movie, char* nombre)
          {
               if(movie[i].ocupado == 1)
               {
-                    sprintf(dinamicAux, "<article class='col-md-4 article-intro'><a href='#'><img class='img-responsive img-rounded' src='%s' alt=''></a><h3><a href='#'>%s</a></h3><ul><li>Género:%s</li><li>Puntaje:%d</li><li>Duración:%d</li></ul><p>%s</p></article>", movie[i].linkImagen, movie[i].titulo, movie[i].genero, movie[i].puntaje, movie[i].duracion, movie[i].descripcion);
+                    sprintf(dinamicAux, "<article class='col-md-4 article-intro'><a href='#'><img class='img-responsive img-rounded' src='%s' alt=''></a><h3><a href='#'>%s</a></h3><ul><li>Genero:%s</li><li>Puntaje:%d</li><li>Duracion:%d</li></ul><p>%s</p></article>", movie[i].linkImagen, movie[i].titulo, movie[i].genero, movie[i].puntaje, movie[i].duracion, movie[i].descripcion);
 
                     strcat(dinamico, dinamicAux);
 
@@ -274,23 +276,13 @@ void generarPagina(EMovie* movie, char* nombre)
 
          if(hayDatos == 1)
          {
-             pFijoUno = fopen("fijoUno.txt", "r"); /**< Primera parte HTML */
-             fread(fijoUno, sizeof(char) , strlen(fijoUno) , pFijoUno);
-             pFijoDos = fopen("fijoDos.txt", "r"); /**< Ultima parte HTML */
-             fread(fijoDos, sizeof(char) , strlen(fijoDos) , pFijoDos);
+             fwrite(fijoUno, sizeof(fijoUno), 1, pArch); //
+             fwrite(dinamico, sizeof(dinamico), 1, pArch);
+             fwrite(fijoDos, sizeof(fijoDos), 1, pArch);
 
-             strcat(fijoUno, dinamico);
-             strcat(fijoDos, fijoUno);
-             strcat(textoFinal, fijoDos);
-
-             fprintf(pArch, "%s", textoFinal); //
-
-             fclose(pFijoUno);
-             fclose(pFijoDos);
+             printf("\n\n~Pagina web generada exitosamente!\n\n");
          }
 
       }
-
       fclose(pArch);
-
 }
